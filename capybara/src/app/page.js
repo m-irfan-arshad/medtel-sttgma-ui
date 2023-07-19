e8ee20f4dc2bb01e9df2d360d6932e79dedf269d
m_irfan_arshad <mirfanarshad17@gmail.com>
2023-07-19 11:31:02 -0400

    grid o' clock

--- capybara/src/app/page.js
+++ capybara/src/app/page.js
@@ -2,17 +2,40 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import BasicSelect from './BasicSelect'
 import { Select } from '@mui/material'
+import React from 'react';
+import Grid from '@mui/material/Grid';
+
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <BasicSelect> </BasicSelect>
-      </div>
+    <main>
+      <div
+        className={styles.gradientBackground}
+        // You can adjust the gradient colors and angle as needed
+        style={{
+          background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+          borderRadius: '8px', // Optional: add border-radius for rounded corners
+          padding: '20px', // Optional: add padding to the container
+        }}
+      >
+
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <BasicSelect> </BasicSelect>
+        </Grid>
+        <Grid item xs={6}>
+          <BasicSelect> </BasicSelect>
+        </Grid>
+        <Grid item xs={6}>
+          <BasicSelect> </BasicSelect>
+        </Grid>
+        <Grid item xs={6}>
+          <BasicSelect> </BasicSelect>
+        </Grid>
+      </Grid>
+      
+      
+      </div> {/* end gradient :) */}
     </main>
   )
 }
