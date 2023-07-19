3f3eee377b48cebcdecfaa786d6bbe9fee89ccfe
m_irfan_arshad <mirfanarshad17@gmail.com>
2023-07-19 11:42:44 -0400

    just adding the import statement

--- capybara/src/app/page.js
+++ capybara/src/app/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import BasicSelect from './BasicSelect'
+import BasicDatePicker from './BasicDatePicker'
 import { Select } from '@mui/material'
 
 import React from 'react';
