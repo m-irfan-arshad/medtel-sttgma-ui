fc8753907e1de9626c891ae6421f8f763c2961ea
m_irfan_arshad <mirfanarshad17@gmail.com>
2023-07-31 16:58:10 -0400

    hello

--- capybara/src/app/STTGMA_Score.jsx
+++ capybara/src/app/STTGMA_Score.jsx
@@ -5,22 +5,59 @@ import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 
 export default function STTGMA_Score(props){
-	const {age, gcs_total, ais_head_neck,ais_chest,cci_index,amb_status,covid_value,asa} = props
+	const {age, gcs_total, ais_head_neck,ais_chest,cci_index,amb_status,covid_value,asa, setSttgma, sttgmaScore, risk_group, setRisk} = props
+	const [buttonColor, setButtonColor] = useState('#eaecef');
+	const [textColor, setTextColor] = useState('black');
 	const [rounded_sttgma, setRoundedSttgma] = useState(null);
-  	const [showResult, setShowResult] = useState(false);
+
 
 	const calculateSTTGMA = (age, gcs_total, ais_head_neck,ais_chest,cci_index,amb_status,covid_value,asa) => {
 		const sttgma_power = -1 * (-15.76 + 0.026 * age + 0.207 * gcs_total + 0.354 * ais_head_neck + 0.441 * ais_chest + 0.165 * cci_index + 0.335 * amb_status + 2.727 * covid_value + 1.65 * asa);
 		const sttgma_e_val = Math.exp(sttgma_power);
 		const sttgma = 1/(1+ sttgma_e_val)
 		setRoundedSttgma(sttgma.toFixed(5));
+		setSttgma(sttgma)
+		calculateRiskGroup()
 
       };
 	
 	  const handleCalculateClick = () => {
 		calculateSTTGMA(age, gcs_total, ais_head_neck, ais_chest, cci_index, amb_status, covid_value, asa);
-		console.log({rounded_sttgma})
 	  };
+
+	  const calculateRiskGroup = () => {             
+			const sttgma_percent = sttgmaScore * 100                                       
+			if (sttgma_percent < 0.24)
+			{
+				setButtonColor("#8feba8")
+				setTextColor ("black")
+				setRisk("Q1: Low Risk");
+			}
+			else if (sttgma_percent < 0.68)
+			{
+				setButtonColor("#fff07a")
+				setTextColor ("black")
+				setRisk("Q2: Mild Risk");
+			}
+			else if (sttgma_percent < 1.32)
+			{
+				setButtonColor("#ff8d30")
+				setTextColor ("black")
+				setRisk("Q3: Moderate Risk");
+			}
+			else if (sttgma_percent <6.90)
+			{
+				setButtonColor("#ff765e")
+				setTextColor ("black")
+				setRisk("Q4: Highest Risk");
+			}
+			else 
+			{
+				setButtonColor("#ad0000")
+				setTextColor ("white")
+				setRisk("Top 5%: Extreme Risk");
+			}
+	  }
 
 	
 
@@ -52,11 +89,11 @@ export default function STTGMA_Score(props){
                 <Grid item xs={3.5}>
                     <TextField
                       fullWidth id="outlined-read-only-input"
-                      defaultValue="Risk Level"
-
+					  value = {risk_group}
                       InputProps={{
                         readOnly: true,
-                        sx: { backgroundColor: '#eaecef'},
+                        sx: { backgroundColor: buttonColor,
+							color: textColor},
                       }}
                       />
                 </Grid>
