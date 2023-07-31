b29737d1e243329c10275157937279e4fb63de39
m_irfan_arshad <mirfanarshad17@gmail.com>
2023-07-31 11:21:12 -0400

    phew

--- capybara/src/app/AgeCalculator.jsx
+++ capybara/src/app/AgeCalculator.jsx
@@ -41,26 +41,24 @@ export default function AgeCalculator(){
     }
   },[dob])
 
-    return(
-    <div style={{justifyContent: 'center'}}>
-    <Grid container spacing={2} justifyContent = "center" alignItems="center" style={{ marginTop: '15px' }}>
-          <Grid item xs={5.25}>
-            <BasicDatePicker dob = {dob} handleDOBChange={handleDOBChange}></BasicDatePicker>
-          </Grid>
-          <Grid item xs={5.25}>
-            <TextField
-            fullWidth id="outlined-read-only-input"
-            defaultValue={age}
-            label = "age"
-
-            InputProps={{
-              readOnly: true,
-              sx: { backgroundColor: '#F5F5F5'},
-            }}
-            />
-
-          </Grid>
+  return(
+  <div style={{ display: 'flex', justifyContent: 'center' ,width: '100%' }}>
+    <Grid container spacing={2} justifyContent = "center" alignItems={"center"}  style={{ marginTop: '15px', marginRight: '4px',marginLeft: '4px'  }}>
+      <Grid item xs={5.25}>
+        <BasicDatePicker></BasicDatePicker>
       </Grid>
-    </div>
-    )
+      <Grid item xs={5.25}>
+        <TextField
+          fullWidth id="outlined-read-only-input"
+          defaultValue= {age}
+          label = "age"
+          InputProps={{
+            readOnly: true,
+            sx: { backgroundColor: '#eaecef'},
+          }}
+        />
+      </Grid>
+  </Grid>
+  </div>
+  )
 }
