9162f2802de33dcf0720a9dc5dabbadc434c4a53
m_irfan_arshad <mirfanarshad17@gmail.com>
2023-08-03 13:22:07 -0400

    imports

--- capybara/src/app/components/procedure/FractureForm.jsx
+++ capybara/src/app/components/procedure/FractureForm.jsx
@@ -1,7 +1,7 @@
 //calls all the drop downs in the procedure box
 "use client"
 import React, { useState } from 'react';
-import Grid  from '@mui/material/Grid';
+import {Grid, TextField}  from '@mui/material';
 import FractureLocationSelect from './FractureLocationSelect';
 import FractureTypeSelect from './FractureTypeSelect';
 import ProcedureSelect from './ProcedureSelect';
@@ -108,7 +108,15 @@ export default function FractureForm({risk_group}) {
           backgroundColor = {backgroundColor}
           onChange = {handleProcedureChoice}
         />
-      </Grid>        
+      </Grid>  
+
+      <TextField
+          id="outlined-multiline-static"
+          label="Multiline"
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+      />      
           
     </Grid>
   );
